b45e15179a660d8c4b85bafa80ec6570ee405b8f
vovj11 <vovj11@hotmail.com>
2024-04-29 01:16:56 -0300

    Adicionado pagina de visualização de perfil, página de edição de posts, funcionalidade de edição e exclusão de posts

--- Forum/src/App.jsx
+++ Forum/src/App.jsx
@@ -1,28 +1,78 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Posts from './Posts';
 import PerfilAccess from './PerfilAccess';
 import NewPost from './NewPost';
 import SearchPost from './SearchPost';
+import EditPost from './EditPost';
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Posts');
   const [loggedInUser, setLoggedInUser] = useState(null);
+  const [postsData, setPostsData] = useState(null);
+  const [postToEdit, setPostToEdit] = useState(null);
+
+  const UrlDataBase =
+    'https://forum-gamificado-postdata-default-rtdb.firebaseio.com/';
 
   const handleLoginSuccess = (user) => {
-    console.log('Dados do usuário recebidos:', user);
     setLoggedInUser(user);
-    console.log('Login bem-sucedido!');
-    console.log('loggedInUserHeader após login:', loggedInUser);
+  };
+
+  function convertData(data) {
+    const ids = Object.keys(data);
+    let posts = Object.values(data);
+    return posts.map((post, index) => {
+      return {
+        id: ids[index],
+        ...post,
+      };
+    });
+  }
+
+  const fetchPosts = async () => {
+    try {
+      const response = await fetch(`${UrlDataBase}/PostsData.json`);
+      const data = await response.json();
+      if (data) {
+        const convertedPosts = convertData(data);
+        setPostsData(convertedPosts);
+      } else {
+        setPostsData([]);
+      }
+    } catch (error) {
+      console.error('Erro ao buscar os tópicos:', error);
+      setPostsData([]);
+    }
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
+
+  const editPostHandler = (postId) => {
+    const postToEdit = postsData.find((post) => post.id === postId);
+    if (postToEdit) {
+      setPostToEdit(postToEdit);
+      setCurrentPage('EditPost');
+    }
   };
 
   const renderPage = () => {
     switch (currentPage) {
       case 'Posts':
-        return <Posts />;
+        return <Posts postsData={postsData} />;
       case 'PerfilAccess':
-        return <PerfilAccess user={loggedInUser} />;
+        return (
+          <PerfilAccess
+            user={loggedInUser}
+            posts={postsData}
+            fetchPosts={fetchPosts}
+            setCurrentPage={setCurrentPage}
+            editPostHandler={editPostHandler}
+          />
+        );
       case 'NewPost':
         return (
           <NewPost
@@ -32,8 +82,25 @@ function App() {
         );
       case 'SearchPost':
         return <SearchPost />;
+      case 'EditPost':
+        return (
+          <EditPost
+            postId={postToEdit?.id}
+            postData={postToEdit}
+            onUpdate={(postId, updatedData) => {
+              fetch(`${UrlDataBase}/PostsData/${postId}.json`, {
+                method: 'PATCH',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(updatedData),
+              }).then(() => {
+                fetchPosts();
+                setCurrentPage('Posts');
+              });
+            }}
+          />
+        );
       default:
-        return <Posts />;
+        return <Posts postsData={postsData} />;
     }
   };
 
--- Forum/src/PerfilAccess.jsx
+++ Forum/src/PerfilAccess.jsx
@@ -1,22 +1,168 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import EditPost from './EditPost';
 
-const ConstructionPageContainer = styled.div`
+const UserInfoContainer = styled.div`
+  margin-bottom: 20px;
+  padding: 16px;
+  border: 1px solid #ddd;
+  border-radius: 8px;
+  background-color: #3c3c3c;
+`;
+
+const UserInfoHeading = styled.h2`
+  color: #6a5acd;
+  margin-bottom: 8px;
+`;
+
+const UserInfoItem = styled.p`
+  margin-bottom: 4px;
+`;
+
+const TableContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const TableHead = styled.thead`
+  background-color: #6a5acd;
+  color: white;
+`;
+
+const TableBody = styled.tbody``;
+
+const TableRow = styled.tr`
+  &:nth-child(even) {
+    background-color: #f8f8f8;
+    color: #000;
+  }
+`;
+
+const TableCell = styled.td`
+  padding: 12px;
+  border: 1px solid #ddd;
+`;
+
+const ButtonGroup = styled.div`
   display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
+  justify-content: space-around;
 `;
 
-const Text = styled.h1`
-  font-size: 3rem;
+const EditButton = styled.div`
+  background-color: #6a5acd;
+  color: white;
+  padding: 6px 12px;
+  border-radius: 4px;
+  text-decoration: none;
+
+  &:hover {
+    background-color: #9400d3;
+  }
 `;
 
-const PerfilAccess = () => {
+const DeleteButton = styled.button`
+  background-color: #ff6347;
+  color: white;
+  padding: 6px 12px;
+  border-radius: 4px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #e83f33;
+  }
+`;
+
+const PerfilAccess = ({ user, posts, fetchPosts, editPostHandler }) => {
+  const [userPosts, setUserPosts] = useState([]);
+  const [editingPostId, setEditingPostId] = useState(null);
+  const [editPostData, setEditPostData] = useState(null);
+  const UrlDataBase =
+    'https://forum-gamificado-postdata-default-rtdb.firebaseio.com/';
+
+  useEffect(() => {
+    setUserPosts(posts.filter((post) => post.creator === user.name));
+  }, [user, posts]);
+
+  const handleDelete = async (postId) => {
+    const confirmation = window.confirm(
+      'Você tem certeza que deseja excluir este post?',
+    );
+
+    if (confirmation) {
+      try {
+        await fetch(`${UrlDataBase}/PostsData/${postId}.json`, {
+          method: 'DELETE',
+        });
+        fetchPosts();
+      } catch (error) {
+        console.error('Erro ao excluir o post:', error);
+      }
+    }
+  };
+
+  const handleEdit = (postId) => {
+    const postToEdit = userPosts.find((post) => post.id === postId);
+    if (postToEdit) {
+      setEditingPostId(postId);
+      setEditPostData(postToEdit);
+      setCurrentPage('EditPost');
+    }
+  };
+
   return (
-    <ConstructionPageContainer>
-      <Text>Página em construção</Text>
-    </ConstructionPageContainer>
+    <>
+      <UserInfoContainer>
+        <UserInfoHeading>Dados do Usuário</UserInfoHeading>
+        <UserInfoItem>
+          <strong>Nome:</strong> {user.name}
+        </UserInfoItem>
+        <UserInfoItem>
+          <strong>Email:</strong> {user.email}
+        </UserInfoItem>
+        <UserInfoItem>
+          <strong>Pontuação:</strong> {user.score}
+        </UserInfoItem>
+      </UserInfoContainer>
+
+      <div>
+        <h2>Posts do Usuário</h2>
+        <TableContainer>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Título</TableCell>
+                <TableCell>Número de Curtidas</TableCell>
+                <TableCell>Número de Descurtidas</TableCell>
+                <TableCell>Opções</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {userPosts.map((post) => (
+                <TableRow key={post.id}>
+                  <TableCell>{post.topic}</TableCell>
+                  <TableCell>{post.like}</TableCell>
+                  <TableCell>{post.dislikes}</TableCell>
+                  <TableCell>
+                    <ButtonGroup>
+                      <EditButton onClick={() => editPostHandler(post.id)}>
+                        Editar
+                      </EditButton>
+                      <DeleteButton onClick={() => handleDelete(post.id)}>
+                        Excluir
+                      </DeleteButton>
+                    </ButtonGroup>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </>
   );
 };
 
